51b7056ec7fd7b47b2a93a51a06566eb0b1e002d
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-08-30 21:35:47 -0400

    added serializers and views to quiz app

--- quiz/models.py
+++ quiz/models.py
@@ -4,17 +4,20 @@ from django.db import models
 
 
 class Exam(models.Model):
-    name = models.CharField()
-    description = models.CharField()
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Question(models.Model):
+    exam = models.ForeignKey('Exam', on_delete=models.CASCADE)
     description = models.TextField(blank=False, unique=True)
     explanation = models.TextField(blank=False)
-    exam = models.ForeignKey('Exam', on_delete=models.CASCADE)
+    is_multiple_choice = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Answer(models.Model):
     question = models.ForeignKey('Question', on_delete=models.CASCADE)
-    is_multiple_choice = models.BooleanField()
     is_correct = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Question
+from .serializers import QuizSerializer
+from rest_framework import generics
 # Create your views here.
+
+
+class QuizListCreate(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuizSerializer
