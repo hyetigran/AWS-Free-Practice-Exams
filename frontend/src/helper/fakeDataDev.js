ed34804101c178cf9076b0c466869a4836739dd9
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-08-31 01:33:23 -0400

    added fake exam data for dev

---
+++ frontend/src/helper/fakeDataDev.js
@@ -0,0 +1,75 @@
+//fake exam data for development purposes
+
+const fakeData = [
+  {
+    question: "first question",
+    isMultipleChoice: true,
+    choices: [
+      { choice: "first choice", isCorrect: true },
+      { choice: "second choice", isCorrect: false },
+      { choice: "third choice", isCorrect: false },
+      { choice: "fourth choice", isCorrect: false },
+    ],
+    explanation:
+      "this is the explanation of why choice is correct for the first question",
+    examNumber: 1,
+    examName: "AWS CCP",
+  },
+  {
+    question: "second question",
+    isMultipleChoice: true,
+    choices: [
+      { choice: "first choice", isCorrect: true },
+      { choice: "second choice", isCorrect: false },
+      { choice: "third choice", isCorrect: false },
+      { choice: "fourth choice", isCorrect: false },
+    ],
+    explanation:
+      "this is the explanation of why choice is correct for the second question",
+    examNumber: 1,
+    examName: "AWS CCP",
+  },
+  {
+    question: "third question",
+    isMultipleChoice: true,
+    choices: [
+      { choice: "first choice", isCorrect: true },
+      { choice: "second choice", isCorrect: false },
+      { choice: "third choice", isCorrect: false },
+      { choice: "fourth choice", isCorrect: false },
+    ],
+    explanation:
+      "this is the explanation of why choice is correct for the third question",
+    examNumber: 1,
+    examName: "AWS CCP",
+  },
+  {
+    question: "fourth question",
+    isMultipleChoice: true,
+    choices: [
+      { choice: "first choice", isCorrect: true },
+      { choice: "second choice", isCorrect: false },
+      { choice: "third choice", isCorrect: false },
+      { choice: "fourth choice", isCorrect: false },
+    ],
+    explanation:
+      "this is the explanation of why choice is correct for the fourth question",
+    examNumber: 1,
+    examName: "AWS CCP",
+  },
+  {
+    question: "fith question",
+    isMultipleChoice: false,
+    choices: [
+      { choice: "first choice", isCorrect: true },
+      { choice: "second choice", isCorrect: false },
+      { choice: "third choice", isCorrect: false },
+      { choice: "fourth choice", isCorrect: true },
+      { choice: "fith choice", isCorrect: false },
+    ],
+    explanation:
+      "this is the explanation of why choice is correct for the fith question",
+    examNumber: 1,
+    examName: "AWS CCP",
+  },
+];
